a8e3547e66bbcd9c84f982ae247801a1139c8d46
Mikolaj Kornas <mikolaj.kornas@gmail.com>
2022-03-01 10:03:41 +0100

    Build method switched to cmake

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "src/position.h"
+#include "src/movegenerators.h"
+#include "src/functions.h"
+#include "src/node.h"
+#include "src/move.h"
+#include "src/search.h"
+#include "src/evaluate.h"
+#include "src/uci.h"
+
+int main(int argc, char **argv){
+    if(argc == 1){
+        freopen("/dev/null", "w", stderr);
+    }
+    Init();
+    SearchTree::Init();
+    Uci::loop();
+    Cleanup();
+}
