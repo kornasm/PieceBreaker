8ea33ee008ed167523e574d80ddaab9ef245787f
Mikolaj Kornas <mikolaj.kornas@gmail.com>
2022-06-16 23:44:06 +0200

    Integrated google test framework
    Simple tests added

---
+++ tests/fen_test.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#include "../src/functions.h"
+#include "../src/node.h"
+
+TEST(BoardFunctions, BoardIndicies){
+    std::string notation = "a1";
+    std::vector<int> rows = {1, 5, 4, 8, 1, 2, 3, 4, 6, 1, 8};
+    std::vector<int> columns = {1, 1, 2, 2, 3, 3, 3, 5, 6, 8, 8};
+    std::vector<int> indicies = {22, 62, 53, 93, 24, 34, 44, 56, 77, 29, 99};
+    std::vector<std::string> notations = {"a1", "a5", "b4", "b8", "c1", "c2", "c3", "e4", "f6", "h1", "h8"};
+    for(int i = 0; i < indicies.size(); i++){
+        EXPECT_EQ(Not2Ind(notations[i]), indicies[i]);
+        EXPECT_EQ(Ind2Not(indicies[i]), notations[i]);
+        EXPECT_EQ(columns[i], column(indicies[i]));
+        EXPECT_EQ(rows[i], row(indicies[i]));
+    }
+}
+
+TEST(PositionTests, FenReadingPrinting){
+    Init();
+    std::vector<std::string> fens = { "rn3bn1/pbppk1pp/1p2p2r/2q1Np2/8/2P1PK2/P1PP1PPP/RNBQ1B1R w - - 0 1",
+                                      "4k3/Q7/4K3/8/8/8/8/8 w - - 0 1",
+                                      "4k3/Q7/4K3/8/8/8/8/8 b - - 10 115",
+                                      "rnbqkb1r/pp3ppp/3p1n2/2pPp3/4P3/5N2/PPP2PPP/RNBQKB1R w KQkq c6 38 75" };
+    for(auto inFen : fens){
+        std::stringstream sstr(inFen);
+        Node *nd = new Node(sstr);
+        std::string outFen = nd->GetFen();
+        EXPECT_EQ(inFen, outFen) << "Test:  " << inFen << "  failed\n";
+        std::cerr << "#" << inFen << "#\n#" << outFen << "#\n";
+        delete nd;
+    }
+    Cleanup();
+}
+
+TEST(PcbrTests, test2){
+    for(int i = 0; i <= 0; i++){
+        EXPECT_EQ(1, 1);
+    }
+}
